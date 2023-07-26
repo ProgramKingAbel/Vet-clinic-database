ac8c19515e354a99b11c21d66ee13f976d23a131
ProgramKingAbel <abelmorara254@gmail.com>
2023-07-26 01:32:56 +0100

    Add transaction queries

--- queries.sql
+++ queries.sql
@@ -8,3 +8,32 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- set species col to unspecified
+UPDATE animals SET species = 'unspecified';
+
+-- perform rollback to set species to initial state of null
+UPDATE animals SET species = NULL;
+
+-- transaction 2
+BEGIN; 
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
+
+--transaction 3
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+-- transaction 4
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT point1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SAVEPOINT point1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
