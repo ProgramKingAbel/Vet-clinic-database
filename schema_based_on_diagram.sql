2a5302d4061ad1291e5118d5ddf67b20c51aed20
Derrick1451 <haylederrick14@gmail.com>
2023-08-02 11:26:34 +0300

    Edit shema file

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -13,3 +13,32 @@ CREATE TABLE invoices (
   medical_history_id INT REFERENCES medical_histories(id),
   PRIMARY KEY(id)
 );
+
+-- Medical histories table 
+
+CREATE TABLE medical_histories (
+  id INT,
+  admitted_at TIMESTAMP,
+  patient_id INT,
+  status VARCHAR(255),
+  PRIMARY KEY (id)
+);
+
+--  treatments table 
+
+CREATE TABLE treatments (
+  id INT,
+  type VARCHAR(255),
+  name VARCHAR(255),
+  PRIMARY KEY (id)
+);
+
+--junction table for many to many relationship 
+
+CREATE TABLE medical_history_treatments (
+  medical_history_id INT,
+  treatment_id INT,
+  PRIMARY KEY (medical_history_id, treatment_id),
+  FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id),
+  FOREIGN KEY (treatment_id) REFERENCES treatments (id)
+);
